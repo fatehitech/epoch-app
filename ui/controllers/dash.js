6e9d83585b3ed7ad072fb51e74f9d6789b4218b2
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-07-27 20:46:28 -0400

    time formatting

--- ui/controllers/dash.js
+++ ui/controllers/dash.js
@@ -10,11 +10,27 @@ angular.module('controllers.dash', [])
 
   $scope.startTime = null;
 
+  function pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
+
   $scope.formatSession = function(session) {
+    var diff = null;
     if (session.end) {
-      return moment(session.end).diff(session.start);
+      diff = moment(session.end).diff(session.start);
     } else {
-      return moment().diff(session.start);
+      diff = moment().diff(session.start);
+    }
+    var duration = moment.duration(diff)
+    var hours = duration.hours()
+    var mins = pad(duration.minutes(), 2)
+    var secs = pad(duration.seconds(), 2)
+    if (hours > 0) {
+      return [hours, mins, secs].join(':')
+    } else {
+      return [mins, secs].join(':')
     }
   }
 
