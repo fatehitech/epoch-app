54f7647aa8d096326113f4517fe96389ab46e0b2
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-07-31 11:45:18 -0400

    set sessions order

--- ui/controllers/project-info.js
+++ ui/controllers/project-info.js
@@ -2,13 +2,19 @@ var moment = require('moment');
 
 angular.module('controllers.project-info', [])
 
+.filter('reverse', function() {
+  return function(items) {
+    return items.slice().reverse();
+  };
+})
+
 .controller('ProjectInfoCtrl', function($stateParams, $scope, Project, Clock) {
   $scope.project = Project.find({ uuid: $stateParams.uuid });
   $scope.clock = Clock.find({ uuid: $scope.project.uuid });
 
   if ($scope.clock.isOn()) {
     $scope.clock.setInterval(function() {
-      $scope.$apply();
+      $scope.$digest();
     });
   }
 
