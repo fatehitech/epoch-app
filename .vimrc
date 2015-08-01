eed522047af28005ce493b97d07e21a2cff8747d
Keyvan Fatehi <keyvanfatehi@gmail.com>
2015-08-01 13:45:30 -0400

    outgrow menubar

---
+++ .vimrc
@@ -0,0 +1,5 @@
+let g:ctrlp_custom_ignore = {
+  \ 'dir':  '\v[\/](\.(git|hg|svn)|lib|node_modules|Epoch-.+)$',
+  \ 'file': '\v\.(exe|so|dll|pyc)$',
+  \ 'link': 'some_bad_symbolic_links',
+  \ }
--- main.js
+++ main.js
@@ -1,31 +1,60 @@
 // https://github.com/maxogden/menubar
 // https://github.com/atom/electron/blob/master/docs/api/browser-window.md
-var menubar = require('menubar')
-var ipc = require('ipc');
+var app = require('app')
+var Tray = require('tray')
+var Menu = require('menu')
+var MenuItem = require('menu-item')
+var ipc = require('ipc')
+var BrowserWindow = require('browser-window')
 
-var mb = menubar({
-  dir: __dirname,
-  preloadWindow: true
-})
+app.on('ready', function ready () {
 
-mb.on('ready', function ready () {
-  // dynamic icon is not supported
-  // https://github.com/maxogden/menubar/issues/23
-  mb.tray.setImage('tray_icons/red.png');
+  win = new BrowserWindow({
+    width: 400,
+    height: 600,
+    show: false,
+  });
+  win.on('closed', function() {
+    win = null;
+  });
+
+  win.webContents.on('did-finish-load', function() {
+    win.show();
+  });
+
+  win.loadUrl('file://'+__dirname+'/index.html');
 
   ipc.on('clockedIn', function(name) {
-    mb.tray.setImage('tray_icons/green.png');
+    tray.setImage(__dirname+'/tray_icons/green.png');
   });
 
   ipc.on('clockedOut', function(name) {
-    mb.tray.setImage('tray_icons/red.png');
+    tray.setImage(__dirname+'/tray_icons/red.png');
   });
 
   ipc.on('terminate', function() {
-    mb.app.terminate()
+    app.terminate()
   });
 
   ipc.on('toggleDevTools', function() {
-    mb.window.toggleDevTools({detach: true});
+    win.toggleDevTools({detach: true});
   })
+
+  win.on('close', function() {
+    win.hide();
+  });
+
+  app.on('window-all-closed', function() {
+    //app.quit();
+  });
+
+  tray = new Tray(__dirname+'/tray_icons/red.png')
+
+  var menu = new Menu();
+  menu.append(new MenuItem({ label: 'Show', click: function() { 
+    win.show()
+  } }));
+
+  tray.setContextMenu(menu);
+
 })
